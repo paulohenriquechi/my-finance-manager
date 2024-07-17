483765f6b22ae6980029a9fa1aae15c3632386be
Paulo Henrique Chi <paulo.henrique.chi@outlook.com>
2024-07-17 00:00:21 -0300

    layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,47 @@ import { z } from "zod"
 import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./components/ui/dialog"
 import { Button } from "./components/ui/button"
 
+const PAYMENT_TYPES = [
+  {
+    label: "Dinheiro",
+    value: "cash"
+  },
+  {
+    label: "Cartão de Crédito",
+    value: "credit_card"
+  },
+  {
+    label: "Cartão de Débito",
+    value: "debit_card"
+  },
+  {
+    label: "TED",
+    value: "ted"
+  },
+  {
+    label: "DOC",
+    value: "doc"
+  },
+  {
+    label: "Boleto Bancário",
+    value: "bank_slip"
+  },
+  {
+    label: "Pix",
+    value: "pix"
+  },
+]
+
 const createExpenseSchema = z.object({
   name: z.string().min(2),
   category: z.coerce.number().min(1, {
     message: "Required category"
   }),
   price: z.coerce.number(),
-  payment_type: z.coerce.number().min(1, {
-    message: "Required payment type"
+  payment_type: z.string().min(1, {
+    message: 'Required payment type'
   }),
-  installments: z.coerce.number().min(1),
+  installments: z.coerce.number().min(1).default(1),
   purchase_date: z.coerce.date(),
 })
 
@@ -24,7 +55,7 @@ export function App() {
   const [isCreditCard, setIsCreditCard] = useState(false)
 
   const displayInstallmentInput = (value: string) => {
-    value == '3' ? setIsCreditCard(true) : setIsCreditCard(false)
+    value == 'credit_card' ? setIsCreditCard(true) : setIsCreditCard(false)
   }
 
   const {
@@ -44,107 +75,114 @@ export function App() {
   }
 
   return (
-    <div className="m-auto">
-      <h1 className="text-5xl">Create Expense</h1>
+    <div className="h-screen">
+      <div className="flex h-full gap-5">
+        <div className="w-64 border border-red-500">
+          <div>
+            <h1 className="text-center">My Finance Manager</h1>
+          </div>
+        </div>
+        <div className="flex-1 border border-red-500">
+          <h2 className="text-2xl p-4">Create Expense</h2>
+          <div className="p-4">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant={"outline"}>Create expense</Button>
+              </DialogTrigger>
 
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button>Create expense</Button>
-        </DialogTrigger>
+              <DialogContent className="max-w-5xl h-2/4">
+                <form onSubmit={handleSubmit(handleCreateExpense)}>
+                  <DialogHeader>
+                    <DialogTitle>Create a new expense</DialogTitle>
+                  </DialogHeader>
 
-        <DialogContent className="max-w-5xl h-2/4">
-          <form onSubmit={handleSubmit(handleCreateExpense)}>
-            <DialogHeader>
-              <DialogTitle>Create a new expense</DialogTitle>
-            </DialogHeader>
+                  <div className="grid grid-cols-12 gap-4 p-6">
 
-            <div className="grid grid-cols-12 gap-4 p-6">
+                    <div className="col-span-6">
+                      <label htmlFor="name" className="block">Expense: </label>
+                      <input {...register('name')} type="text" id="name" placeholder="Name" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" />
+                      {errors.name?.message ?? (
+                        <p>{errors.name?.message}</p>
+                      )}
+                    </div>
 
-            <div className="col-span-6">
-              <label htmlFor="name" className="block">Expense: </label>
-              <input {...register('name')} type="text" id="name" placeholder="Name" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" />
-              {errors.name?.message ?? (
-                <p>{errors.name?.message}</p>
-              )}
-            </div>
+                    <div className="col-span-6">
+                      <label htmlFor="category">Category: </label>
+                      <select {...register('category')} id="category" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full">
+                        <option value="">Select a category</option>
+                        <option value="1">Food</option>
+                        <option value="2">Transport</option>
+                        <option value="3">Clothes</option>
+                      </select>
+                      {errors.category?.message ?? (
+                        <p>{errors.category?.message}</p>
+                      )}
+                    </div>
 
-            <div className="col-span-6">
-              <label htmlFor="category">Category: </label>
-              <select {...register('category')} id="category" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full">
-                <option value="">Select a category</option>
-                <option value="1">Food</option>
-                <option value="2">Transport</option>
-                <option value="3">Clothes</option>
-              </select>
-              {errors.category?.message ?? (
-                <p>{errors.category?.message}</p>
-              )}
-            </div>
-
-            <div className="col-span-4">
-              <label htmlFor="price">Price: </label>
-              <input {...register('price')} type="text" id="price" placeholder="100.00" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" />
-              {errors.price?.message ?? (
-                <p>{errors.price?.message}</p>
-              )}
-            </div>
-
-
-            <div className="col-span-4">
-              <label htmlFor="payment_type">Payment Type</label>
-              <select {...register('payment_type')} id="payment_type" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" onChangeCapture={(e) => displayInstallmentInput(e.currentTarget.value)}>
-                <option value="">Select a payment type</option>
-                <option value="1">Money</option>
-                <option value="2">Pix</option>
-                <option value="3">Credit Card</option>
-                <option value="4">Debit Card</option>
-              </select>
-              {errors.payment_type?.message ?? (
-                <p>{errors.payment_type?.message}</p>
-              )}
-
-            </div>
-
-            {isCreditCard && (
-              <div className="col-span-2">
-                <label htmlFor="installments">Installments</label>
-                <select {...register('installments')} id="installments" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full">
-                  {Array.from({ length: 24 }).map((_, index) => (
-                    <option key={index + 1} value={(index + 1).toString()}>{index + 1}x</option>
-                  ))
-                  }
-                </select>
-                {errors.installments?.message ?? (
-                  <p>{errors.installments?.message}</p>
-                )}
-              </div>
-            )}
+                    <div className="col-span-4">
+                      <label htmlFor="price">Price: </label>
+                      <input {...register('price')} type="text" id="price" placeholder="100.00" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" />
+                      {errors.price?.message ?? (
+                        <p>{errors.price?.message}</p>
+                      )}
+                    </div>
 
 
+                    <div className="col-span-4">
+                      <label htmlFor="payment_type">Payment Type</label>
+                      <select {...register('payment_type')} id="payment_type" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" onChangeCapture={(e) => displayInstallmentInput(e.currentTarget.value)}>
+                        <option value="">Select a payment type</option>
+                        {PAYMENT_TYPES.map((paymentType, index) => (
+                          <option key={index} value={paymentType.value}>{paymentType.label}</option>
+                        ))}
+                      </select>
+                      {errors.payment_type?.message ?? (
+                        <p>{errors.payment_type?.message}</p>
+                      )}
 
-            <div className="col-span-2">
-              <label htmlFor="purchase_date">Purchase Date: </label>
-              <input {...register('purchase_date')} type="date" id="purchase_date" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" />
-              {errors.purchase_date?.message ?? (
-                <p>{errors.purchase_date?.message}</p>
-              )}
+                    </div>
 
-            </div>
-
-            </div>
-
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant={"destructive"}>Cancel</Button>
-              </DialogClose>
-              <Button type="submit">Submit</Button>
-            </DialogFooter>
-          </form>
-
-        </DialogContent>
-      </Dialog>
+                    {isCreditCard && (
+                      <div className="col-span-2">
+                        <label htmlFor="installments">Installments</label>
+                        <select {...register('installments')} id="installments" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full">
+                          {Array.from({ length: 24 }).map((_, index) => (
+                            <option key={index + 1} value={(index + 1).toString()}>{index + 1}x</option>
+                          ))
+                          }
+                        </select>
+                        {errors.installments?.message ?? (
+                          <p>{errors.installments?.message}</p>
+                        )}
+                      </div>
+                    )}
 
 
+
+                    <div className="col-span-2">
+                      <label htmlFor="purchase_date">Purchase Date: </label>
+                      <input {...register('purchase_date')} type="date" id="purchase_date" className="block mt-2 p-1.5 rounded-md border placeholder:text-gray-400 w-full" />
+                      {errors.purchase_date?.message ?? (
+                        <p>{errors.purchase_date?.message}</p>
+                      )}
+
+                    </div>
+
+                  </div>
+
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant={"destructive"} onClick={() => reset()}>Cancel</Button>
+                    </DialogClose>
+                    <Button type="submit">Submit</Button>
+                  </DialogFooter>
+                </form>
+
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
